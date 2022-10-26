0332da2fbc38453cbd73bb74ec66765e3e1fa191
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-26 10:58:51 -0300

    :rocket: feat: create controller ArticleController

---
+++ src/main/java/com/example/elasticstoragejava/controller/ArticleController.java
@@ -0,0 +1,26 @@
+package com.example.elasticstoragejava.controller;
+
+import com.example.elasticstoragejava.model.Article;
+import com.example.elasticstoragejava.service.IArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    @Autowired
+    private IArticleService service;
+
+    @PostMapping
+    public ResponseEntity<Article> insert(@RequestBody Article article) {
+        Article newArticle = service.save(article);
+
+        return new ResponseEntity<>(newArticle, HttpStatus.CREATED);
+    }
+}
