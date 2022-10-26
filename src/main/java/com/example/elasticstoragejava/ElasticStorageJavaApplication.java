2dd5412f03e4ceabcdada3529758a3ddfdbda612
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-26 10:33:25 -0300

    :building_construction: build: initial setup

---
+++ src/main/java/com/example/elasticstoragejava/ElasticStorageJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticstoragejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticStorageJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticStorageJavaApplication.class, args);
+    }
+
+}
