1f593a203be58e98c9b71b4d897c161e7cb85a77
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-26 15:31:31 -0300

    :speech_balloon: feat: add comments about generation automatic the Primary Key

--- src/main/java/com/example/elasticstoragejava/model/Article.java
+++ src/main/java/com/example/elasticstoragejava/model/Article.java
@@ -13,6 +13,8 @@ import java.util.List;
 @Getter
 @Setter
 public class Article {
+
+    // somente conseguimos gerar id aleatorio, se estivermos usando String, pois no bd noSQL usamos hash para gerar essas chves automaricas, sendo do tipo String
     @Id
     private int id;
     private String title;
