e41cdc76fc021778fac1e6fc2f9596151e05642a
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-26 14:59:38 -0300

    :rocket: feat: implements querys create with @annotation @Querys in Repository

--- src/main/java/com/example/elasticstoragejava/repository/ArticleRepository.java
+++ src/main/java/com/example/elasticstoragejava/repository/ArticleRepository.java
@@ -1,10 +1,13 @@
 package com.example.elasticstoragejava.repository;
 
 import com.example.elasticstoragejava.model.Article;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
 
 public interface ArticleRepository extends ElasticsearchRepository<Article, Integer> {
+    // usando querys create
+    @Query("{\"match_all\": {}}")
     List<Article> findAll();
 }
