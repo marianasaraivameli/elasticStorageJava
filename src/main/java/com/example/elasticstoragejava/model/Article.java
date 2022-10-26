249f48f743cd0fadf500c009546e00d5810629d5
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-26 10:56:07 -0300

    :rocket: feat: create model Article

---
+++ src/main/java/com/example/elasticstoragejava/model/Article.java
@@ -0,0 +1,15 @@
+package com.example.elasticstoragejava.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "blog")
+@Getter
+@Setter
+public class Article {
+    @Id
+    private int id;
+    private String title;
+}
